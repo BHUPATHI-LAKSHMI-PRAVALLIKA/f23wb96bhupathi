928def4a02224c2610ad791dda8efaf021c78955
BHUPATHI-LAKSHMI-PRAVALLIKA <77114989+BHUPATHI-LAKSHMI-PRAVALLIKA@users.noreply.github.com>
2023-10-30 19:01:29 -0500

    file updated

--- routes/board.js
+++ routes/board.js
@@ -1,42 +1,23 @@
 const express = require('express');
-
 const router = express.Router();
-
-router.get('/', (req, res, next) => {
-
- const rows = Number(req.query.rows);
-
- const cols = Number(req.query.cols);
-
- if (!Number.isInteger(rows) || !Number.isInteger(cols)) {
-
-  res.render('board', { title: 'Board Display', errorMessage: 'Query parameters must be integers.' });
-
- } else if (rows < 1 || rows > 10) {
-
-  res.render('board', { title: 'Board Display', errorMessage: 'Rows out of range. Must be between 1 and 10.' });
-
- } else if (cols < 1 || cols > 10) {
-
-  res.render('board', { title: 'Board Display', errorMessage: 'Columns out of range. Must be between 1 and 10.' });
-
+function computeCellValue(row, col) {
+ if (row === col) {
+  return 1;
+ } else if (row === col + 1) {
+  return 2;
+ } else if (row === col - 1) {
+  return row;
  } else {
-
-  const tableData = generateTableData(rows, cols);
-
-  res.render('board', { title: 'Board Display', tableData });
-
+  return 0;
  }
-
-});
-
-function generateTableData(rows, cols) {
-
- // Generate your table data based on the rows and cols
-
- // Return the data as needed for your Pug template
-
 }
-
+router.get('/', (req, res, next) => {
+ const rows = Number(req.query.rows);
+ const cols = Number(req.query.cols);
+ if (!Number.isInteger(rows) || !Number.isInteger(cols) || rows < 1 || rows > 10 || cols < 1 || cols > 10) {
+  res.render('board', { title: 'Board Display', errorMessage: 'Invalid query parameters.' });
+ } else {
+  res.render('board', { title: 'Board Display', query: req.query, computeCellValue });
+ }
+});
 module.exports = router;
-
